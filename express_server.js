07daa0f6a58398aaf1d8446dee8497365a4c1789
caro-turnbull <caro.turnbull@gmail.com>
2023-06-14 15:41:25 -0400

    changed to user_id cookie from registration page, changed on header aswell

--- express_server.js
+++ express_server.js
@@ -3,14 +3,33 @@ const cookieParser = require("cookie-parser")
 const app = express()
 const PORT = 8080
 
+//Is this middleware?
 app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
+
+// Object "Databses"
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+
+//Functions
 
 function generateRandomString(){
   const characters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890"
@@ -20,6 +39,9 @@ function generateRandomString(){
   }
   return result
 }
+
+
+//Routes
 
 app.get("/", (req,res) => {
   res.send("Hello!");
@@ -34,40 +56,13 @@ app.get("/urls.json", (req,res) => {
 // });
 
 app.get("/urls", (req, res) => {               //url list page 
-  const templateVars = {                       //passed variables database and username
+  const templateVars = {                       //passed variables urldatabase and username
     urls: urlDatabase,  
-    username: req.cookies["username"]
+    //userObj: users,
+    user_id: res.cookie.user_id
   };
   res.render("urls_index", templateVars);
 });
-
-app.get("/urls/new", (req, res) => {         //create new
-  const templateVars = {  
-    username: req.cookies["username"]
-  };
-  res.render("urls_new", templateVars);
-});
-
-app.get("/urls/:id", (req, res) => {          //indv entry pages
-  const templateVars = { 
-    id: req.params.id, 
-    longURL: urlDatabase[req.params.id],
-    username: req.cookies["username"]
-  }
-  res.render("urls_show", templateVars)
-});
-
-app.post("/urls/:id", (req, res) => {
-  //req.body is what is sent from the form, req.body.longURL is the new value
-  console.log("req.body", req.body); 
-  // what is the existing key? req.params.id
-  console.log("req.params.id", req.params.id)
-  // edit urlDatabase object to update the existing key with the new value
-  urlDatabase[req.params.id] = req.body.longURL
-  // redirect back to index page
-  res.redirect(`/urls/${req.params.id}`)
-  
-})
 
 app.post("/urls", (req, res) =>{             //create a new entry
   let id = generateRandomString();
@@ -78,15 +73,13 @@ app.post("/urls", (req, res) =>{             //create a new entry
   //console.log(urlDatabase)  <--not all database keys are the same type??
 })
 
-app.get ("/u/:id", (req,res) =>{        //redirects the shortest url to the actual page
-  const longURL = urlDatabase[req.params.id]     //brackets means the value in the param
-  res.redirect(longURL)
-})
-
-app.post("/urls/:id/delete", (req, res) => {  //delete an entry
-  delete urlDatabase[req.params.id]
-  res.redirect("/urls")
-} )
+app.get("/urls/new", (req, res) => {         //create new
+  const templateVars = {  
+    //userObj: users
+    user_id: res.cookie.user_id
+  };
+  res.render("urls_new", templateVars);
+});
 
 app.post("/login", (req, res) => {
   res.cookie('username', req.body.name)  //creates a cookie called username, with the value from the form
@@ -101,11 +94,61 @@ app.post("/logout", (req, res) =>{
 
 app.get("/register", (req, res) => {
   const templateVars = {  
-    username: req.body.email,
-    password: req.body.password
+    user_id: res.cookie.user_id,
   };
   res.render("registration", templateVars);
 })
+
+app.post("/register", (req, res) => {
+  let id = generateRandomString();
+  //console.log(req.body, id);
+  users[id] = {             //creates a new entry in the users object "database"
+    id: id,
+    email: req.body.email,
+    password:  req.body.password
+  }
+  for (let userID in users) {   
+    console.log(users[userID])
+ //   if users[userID].email ===email)    //for later
+
+  }
+  res.cookie('user_id', id)   //creates a cookie called user_id, with the value from the random function
+  //console.log(`user object: ${res.cookie.user_id}`)  //??Test that the users object is properly being appended to
+  res.redirect("/urls")
+  
+})
+
+
+app.get("/urls/:id", (req, res) => {          //indv entry pages
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id],
+    user_id: res.cookie.user_id
+  }
+  res.render("urls_show", templateVars)
+});
+
+app.post("/urls/:id", (req, res) => {
+  //req.body is what is sent from the form, req.body.longURL is the new value
+  console.log("req.body", req.body); 
+  // what is the existing key? req.params.id
+  console.log("req.params.id", req.params.id)
+  // edit urlDatabase object to update the existing key with the new value
+  urlDatabase[req.params.id] = req.body.longURL
+  // redirect back to index page
+  res.redirect(`/urls/${req.params.id}`)
+})
+
+app.post("/urls/:id/delete", (req, res) => {  //delete an entry
+  delete urlDatabase[req.params.id]
+  res.redirect("/urls")
+} )
+
+app.get ("/u/:id", (req,res) =>{        //redirects the shortest url to the actual page
+  const longURL = urlDatabase[req.params.id]     //brackets means the value in the param
+  res.redirect(longURL)
+})
+
 
 // app.get("/set", (req, res) => {
 //   const a = 1
